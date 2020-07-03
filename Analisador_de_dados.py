f1aebd8ff1fd29095c196bb36d8084a1e255426d
Corno <thi.araujo.tec@gmail.com>
2020-07-03 19:58:29 -0300

    Programa que analisar dados

---
+++ Analisador_de_dados.py
@@ -0,0 +1,7 @@
+"""
+Programa: Analisador de dados
+Autor: Thiago Lira
+Verção: 3.8.3
+Finalizado: 03/07/2020
+Descrição: Programa quer pergunta ao o usuário o nome, idade, sexo e dizer a média das idade do grupo.
+"""
