910c4f5b6b836396be04e6bb878dba387bf68337
thiagolira <thi.araujo.tec@gmail.com>
2020-07-04 13:22:20 -0300

    Jogo em andamento

--- pedra_papel_tesoura_jogo.py
+++ pedra_papel_tesoura_jogo.py
@@ -6,8 +6,41 @@ Fazendo: 04/07/2020
 Descrição: Um pequeno jogo pedra papel e tesoura, em outras regioes conhecido como Jokenpô.
 """
 
-import random
+from random import *
+jogo = ('pedra', 'papel', 'tesoura')
+maquina = randint(0, 2)
 
-jogo = ['pedra', 'papel', 'tesoura']
+print('Olá, vamos brincar')
+print('Escolha uma das opção!')
+print("""
+    [0]: Pedra
+    [1]: Papel
+    [2]: Tesoura
+""")
 
-maquina = random.choice(jogo)
+usuario = int(input('Qual vocẽ escolhe? '))
+
+print('O jogador escolheu {}!'.format(jogo[usuario]))
+print('O computador escolheu {}!'.format(jogo[maquina]))
+
+if maquina == 0: #Maquina escolheu pedra.
+    if usuario == 0:
+        print('Empate')
+    elif usuario == 1:
+        print('Você venceu, parabéns!')
+    elif usuario == 2:
+        print('O computador ganhou!')
+elif maquina == 1: #Maquina escolheu papel
+    if usuario == 0:
+        print('O computador ganhou!')
+    elif usuario == 1:
+        print('Empate')
+    elif usuario == 2:
+        print('Você venceu, parabéns!')
+elif maquina == 2: #Maquina escolheu tesoura
+    if usuario == 0:
+        print('Você venceu, Parabéns!')
+    elif usuario == 1:
+        print('O computador ganhou!')
+    elif usuario == 2:
+        print('Empate!')
