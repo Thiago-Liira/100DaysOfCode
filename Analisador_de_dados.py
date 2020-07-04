574374f61ea83abe9efee9485df38e6d897ddd17
Corno <thi.araujo.tec@gmail.com>
2020-07-03 21:22:26 -0300

    calculando a ḿedia das idades

--- Analisador_de_dados.py
+++ Analisador_de_dados.py
@@ -1,7 +1,17 @@
 """
 Programa: Analisador de dados
 Autor: Thiago Lira
-Verção: 3.8.3
-Finalizado: 03/07/2020
+Versão: 3.8.3
+Fazendo: 03/07/2020
 Descrição: Programa quer pergunta ao o usuário o nome, idade, sexo e dizer a média das idade do grupo.
 """
+media = 0
+for i in range(1,4):
+    nome = str(input('Digite seu nome: ')).split()
+    idade = int(input('Digite sua idade: '))
+    sexo = str(input('Digite seu sexo: [M/F]: '))
+    
+    #Média das idades:
+    media += idade
+    Media_calculada = media / i
+print('A média das idades é de: {}'.format(Media_calculada))
