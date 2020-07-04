0cdfb469c164bbee7b7714eb3601a926d3e05c37
thiagolira <thi.araujo.tec@gmail.com>
2020-07-04 10:53:20 -0300

    Levantando a primeira parte do jogo

---
+++ pedra_papel_tesoura_jogo.py
@@ -0,0 +1,13 @@
+"""
+Programa: Jogo pedra papel e tesoura
+Autor: Thiago Lira
+Versão: 3.8.3
+Fazendo: 04/07/2020
+Descrição: Um pequeno jogo pedra papel e tesoura, em outras regioes conhecido como Jokenpô.
+"""
+
+import random
+
+jogo = ['pedra', 'papel', 'tesoura']
+
+maquina = random.choice(jogo)
